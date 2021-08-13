38f1f180a8019af31313294ca757708024fac22c
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-08-12 17:03:15 -0700

    Add project files.

---
+++ CppApp/CppApp/MainWindow.xaml.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace WinRTComponent;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::CppApp::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        // We also have a CustomButton written into the XAML 
+        CustomButton customButton = CustomButton();
+        auto str = customButton.GetText();
+    }
+
+    void MainWindow::myCustomButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myCustomButton().Content(box_value(L"Clicked the Custom Button"));
+    }
+}
---
+++ CppApp/CppApp/MainWindow.xaml.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#include "MainWindow.g.h"
+
+#pragma pop_macro("GetCurrentTime")
+
+namespace winrt::CppApp::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        void myCustomButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::CppApp::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
